64eaa2d60045d7f381c498d6e666334d4de76a57
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2021-01-18 04:01:58 +0545

    added dynamic content

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 var scroll = new SmoothScroll('a[href*="#"]');
 
+fetch ("https://api.github.com/users/sijandahal")
+.then(response => response.json())
+.then(function(data) {
+  document.querySelector('.name').textContent =  data['name'];
+  document.querySelector('.githubimage').src = data['avatar_url']
+  document.querySelector('.bio').textContent = data['bio'];
+  document.querySelector('.following').textContent += data['following'];
+  document.querySelector('.followers').textContent += data['followers'];
+  document.querySelector('.repos').textContent += data['public_repos'];
+  document.querySelector('.githubrepos').href = data['html_url']
+  console.log(data);
+})
--- js/navigation.js
+++ js/navigation.js
@@ -1,31 +1,27 @@
-
 const navlink = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
-const links = document.querySelectorAll (".nav-links li");
+const links = document.querySelectorAll(".nav-links li");
 const nav = document.querySelector('.navbar')
 
-burger.addEventListener('click', function() {
+burger.addEventListener('click', function () {
   navlink.classList.toggle('nav-active');
   burger.classList.toggle('toggle');
 
-//animate links for nav-bar
+  //animate links for nav-bar
 
-      links.forEach((link,index)=>{
-        if(link.style.animation){
-          link.style.animation = "";
-        }
-        else {
-        link.style.animation = `fadeinnav 0.5s ease forwards ${index / 7 + 1}s`;
-        }
-  
-      })
-        //burger animation
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `fadeinnav 0.5s ease forwards ${index / 7 + 1}s`;
+    }
+
+  })
+  //burger animation
 
 });
 
 
-window.addEventListener('scroll', function(){
-  nav.classList.toggle ('sticky', window.scrollY>50)
+window.addEventListener('scroll', function () {
+  nav.classList.toggle('sticky', window.scrollY > 50)
 })
-
-
