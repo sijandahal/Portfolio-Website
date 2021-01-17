fa36b0a4710067b6d90818aef41d42b715ac853a
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2021-01-18 02:48:01 +0545

    added styles fixes

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,8 @@
 
 const navlink = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
-const links = document.querySelectorAll (".nav-links li")
+const links = document.querySelectorAll (".nav-links li");
+const nav = document.querySelector('.navbar')
 
 burger.addEventListener('click', function() {
   navlink.classList.toggle('nav-active');
@@ -20,6 +21,11 @@ burger.addEventListener('click', function() {
       })
         //burger animation
 
-      
 });
+
+
+window.addEventListener('scroll', function(){
+  nav.classList.toggle ('sticky', window.scrollY>50)
+})
+
 
